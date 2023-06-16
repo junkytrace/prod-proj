34c358a8daf2410102a2e5346c4cb8e7a76340c1
junkytrace <nizmikhail@gmail.com>
2023-06-16 22:13:51 +0300

    addchecks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,28 +1,36 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack, { web } from "webpack";
-import { BuildOptions } from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack, { web } from 'webpack';
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const plugins = [];
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin()),
+      plugins.push(
+        new BundleAnalyzerPlugin({
+          openAnalyzer: false,
+        })
+      );
+  }
+  return plugins;
 }
