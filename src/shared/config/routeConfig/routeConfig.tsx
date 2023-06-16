a86a8549353f4168f3e4eaad71c10929ab2cbd2d
junkytrace <nizmikhail@gmail.com>
2023-06-17 00:20:25 +0300

    12313131313

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,18 +1,18 @@
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { NotFoundPage } from "pages/NotFoundPage";
-import { RouteProps } from "react-router-dom";
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import NotFoundPage from 'pages/NotFoundPage';
+import { RouteProps } from 'react-router-dom';
 
 export enum AppRoutes {
-  MAIN = "main",
-  ABOUT = "about",
-  NOT_FOUND = "not_found",
+  MAIN = 'main',
+  ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: "/",
-  [AppRoutes.ABOUT]: "/about",
-  [AppRoutes.NOT_FOUND]: "*",
+  [AppRoutes.MAIN]: '/',
+  [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
